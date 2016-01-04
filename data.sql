8be093ae3c6dd0ce96a4f92a487e25a32536b15a
lanwailan <oocco@126.com>
2016-01-04 21:24:22 +0800

    sql database

---
+++ data.sql
@@ -0,0 +1,139 @@
+﻿SET FOREIGN_KEY_CHECKS = 0;
+
+DROP TABLE IF EXISTS  `lan_sort`;
+CREATE TABLE `lan_sort` (
+  `sid` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `sortname` varchar(255) NOT NULL DEFAULT '',
+  `alias` varchar(200) NOT NULL DEFAULT '',
+  `taxis` int(10) unsigned NOT NULL DEFAULT '0',
+  `pid` int(10) unsigned NOT NULL DEFAULT '0',
+  `description` text NOT NULL,
+  `template` varchar(255) NOT NULL DEFAULT '',
+  PRIMARY KEY (`sid`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS  `lan_blog`;
+CREATE TABLE `lan_blog` (
+  `gid` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL DEFAULT '',
+  `date` bigint(20) NOT NULL,
+  `content` longtext NOT NULL,
+  `excerpt` longtext NOT NULL,
+  `alias` varchar(200) NOT NULL DEFAULT '',
+  `author` int(10) NOT NULL DEFAULT '1',
+  `sortid` int(10) NOT NULL DEFAULT '-1',
+  `type` varchar(20) NOT NULL DEFAULT 'blog',
+  `views` int(10) unsigned NOT NULL DEFAULT '0',
+  `comnum` int(10) unsigned NOT NULL DEFAULT '0',
+  `attnum` int(10) unsigned NOT NULL DEFAULT '0',
+  `top` enum('n','y') NOT NULL DEFAULT 'n',
+  `sortop` enum('n','y') NOT NULL DEFAULT 'n',
+  `hide` enum('n','y') NOT NULL DEFAULT 'n',
+  `checked` enum('n','y') NOT NULL DEFAULT 'y',
+  `allow_remark` enum('n','y') NOT NULL DEFAULT 'y',
+  `password` varchar(255) NOT NULL DEFAULT '',
+  `template` varchar(255) NOT NULL DEFAULT '',
+  PRIMARY KEY (`gid`),
+  KEY `date` (`date`),
+  KEY `author` (`author`),
+  KEY `sortid` (`sortid`),
+  KEY `type` (`type`),
+  KEY `views` (`views`),
+  KEY `comnum` (`comnum`),
+  KEY `hide` (`hide`)
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS  `lan_twitter`;
+CREATE TABLE `lan_twitter` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `content` text NOT NULL,
+  `img` varchar(200) DEFAULT NULL,
+  `author` int(10) NOT NULL DEFAULT '1',
+  `date` bigint(20) NOT NULL,
+  `replynum` int(10) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `author` (`author`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS  `lan_navi`;
+CREATE TABLE `lan_navi` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `naviname` varchar(30) NOT NULL DEFAULT '',
+  `url` varchar(75) NOT NULL DEFAULT '',
+  `newtab` enum('n','y') NOT NULL DEFAULT 'n',
+  `hide` enum('n','y') NOT NULL DEFAULT 'n',
+  `taxis` int(10) unsigned NOT NULL DEFAULT '0',
+  `pid` int(10) unsigned NOT NULL DEFAULT '0',
+  `isdefault` enum('n','y') NOT NULL DEFAULT 'n',
+  `type` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `type_id` int(10) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS  `lan_user`;
+CREATE TABLE `lan_user` (
+  `uid` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(32) NOT NULL DEFAULT '',
+  `password` varchar(64) NOT NULL DEFAULT '',
+  `nickname` varchar(20) NOT NULL DEFAULT '',
+  `role` varchar(60) NOT NULL DEFAULT '',
+  `ischeck` enum('n','y') NOT NULL DEFAULT 'n',
+  `photo` varchar(255) NOT NULL DEFAULT '',
+  `email` varchar(60) NOT NULL DEFAULT '',
+  `description` varchar(255) NOT NULL DEFAULT '',
+  PRIMARY KEY (`uid`),
+  KEY `username` (`username`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS  `lan_options`;
+CREATE TABLE `lan_options` (
+  `option_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `option_name` varchar(255) NOT NULL,
+  `option_value` longtext NOT NULL,
+  PRIMARY KEY (`option_id`),
+  KEY `option_name` (`option_name`)
+) ENGINE=MyISAM AUTO_INCREMENT=61 DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS  `lan_reply`;
+CREATE TABLE `lan_reply` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `tid` int(10) unsigned NOT NULL DEFAULT '0',
+  `date` bigint(20) NOT NULL,
+  `name` varchar(20) NOT NULL DEFAULT '',
+  `content` text NOT NULL,
+  `hide` enum('n','y') NOT NULL DEFAULT 'n',
+  `ip` varchar(128) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`),
+  KEY `gid` (`tid`),
+  KEY `hide` (`hide`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS  `lan_tag`;
+CREATE TABLE `lan_tag` (
+  `tid` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `tagname` varchar(60) NOT NULL DEFAULT '',
+  `gid` text NOT NULL,
+  PRIMARY KEY (`tid`),
+  KEY `tagname` (`tagname`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS  `lan_comment`;
+CREATE TABLE `lan_comment` (
+  `cid` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `gid` int(10) unsigned NOT NULL DEFAULT '0',
+  `pid` int(10) unsigned NOT NULL DEFAULT '0',
+  `date` bigint(20) NOT NULL,
+  `poster` varchar(20) NOT NULL DEFAULT '',
+  `comment` text NOT NULL,
+  `mail` varchar(60) NOT NULL DEFAULT '',
+  `url` varchar(75) NOT NULL DEFAULT '',
+  `ip` varchar(128) NOT NULL DEFAULT '',
+  `hide` enum('n','y') NOT NULL DEFAULT 'n',
+  PRIMARY KEY (`cid`),
+  KEY `gid` (`gid`),
+  KEY `date` (`date`),
+  KEY `hide` (`hide`)
+) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+
+SET FOREIGN_KEY_CHECKS = 1;
+
